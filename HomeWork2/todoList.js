2ab51c6be19a565b9d7ff518b77d98bc4b1fa0ee
EnsarTopal <ensartopal34@gmail.com>
2022-08-19 17:00:46 +0300

    patika.dev_ODEV_2

---
+++ HomeWork2/todoList.js
@@ -0,0 +1,62 @@
+let btnDOM = document.querySelector("#liveToastBtn")
+let listDOM = document.querySelector("#list")
+let taskDOM = document.querySelector("#task")
+let ulDOM = document.getElementsByTagName("li");
+
+
+let veriler = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+console.log(veriler);
+veriler.push(taskDOM.value);
+
+
+
+for (let i = 0; i < ulDOM.length; i++) {
+    let closeButton = document.createElement("span");
+    closeButton.textContent = "\u00D7";
+    closeButton.classList.add("close");
+    closeButton.onclick = removeButton;
+    ulDOM[i].append(closeButton);
+    ulDOM[i].onclick = check;
+}
+
+
+btnDOM.addEventListener('click', elemanEkle)
+
+function check() {
+    this.classList.toggle("checked");
+}
+
+let todoArray = [];
+function elemanEkle() {
+
+    if (taskDOM.value == "") {
+        $(".error").toast("show");
+        veriler.push(taskDOM.value);
+    } else {
+        $(".success").toast("show");
+
+        let liDOM = document.createElement('li')
+        listDOM.appendChild(liDOM);
+        liDOM.innerHTML = taskDOM.value;
+        taskDOM.value = "";
+
+        todoArray.push(liDOM.innerHTML);
+        localStorage.setItem('items', JSON.stringify(todoArray))
+
+        liDOM.onclick = check;
+
+        let closeButton = document.createElement("span");
+        closeButton.textContent = "\u00D7";
+        closeButton.classList.add("close");
+        closeButton.onclick = removeButton;
+
+        liDOM.append(closeButton);
+        listDOM.append(liDOM);
+       
+    }
+}
+
+function removeButton() {
+    this.parentElement.remove();
+}
+
